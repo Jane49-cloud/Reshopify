b166277d5dce6fe793ffc8fb04147adea203eb5d
“jane49-cloud” <janendirangu49@gmail.com>
2023-07-01 23:47:50 +0300

    configure multer and cloudinary

--- server/products/controller.js
+++ server/products/controller.js
@@ -1,4 +1,10 @@
 import Product from "./models.js";
+import multer from "multer";
+import cloudinary from "../config/cloudinary.js";
+import express from "express";
+import authMiddleware from "../users/middleware/auth.js";
+
+const router = express();
 
 //create new product
 
@@ -70,3 +76,37 @@ export const deleteProduct = async (req, res) => {
     console.log(error);
   }
 };
+
+// Update to add images via cloudinary
+
+const storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + this.file.originalname);
+  },
+});
+
+router.post(
+  "/upload",
+  authMiddleware,
+  multer({ storage: storage }).single("file"),
+  async (req, res) => {
+    try {
+      const result = await cloudinary.upload(req.file.path);
+      const productId = req.body.productId;
+      await Product.findByIdAndUpdate(productId);
+      res.send({
+        success: true,
+        message: "image uploaded successfully...",
+        result,
+      });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: "Error uploading images...",
+        result,
+      });
+    }
+  }
+);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./users/routes.js";
 import productRouter from "./products/routes.js";
+import ImageRouter from "./products/controller.js";
 const app = express();
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 
 app.use("/users", userRouter);
 app.use("/products", productRouter);
+app.use("/products", ImageRouter);
 
 const port = 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));

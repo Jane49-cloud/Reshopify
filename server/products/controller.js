f9f1da04d2bf8edda27f42434f360af336b1c751
Jane49-cloud <janendirangu49@gmail.com>
2023-09-09 15:57:33 +0300

    add categoty and age filters

--- server/products/controller.js
+++ server/products/controller.js
@@ -29,13 +29,27 @@ export const createProduct = async (req, res) => {
 
 export const getProducts = async (req, res) => {
   try {
-    const { seller, categories = [], age = [], status } = req.body;
+    const { seller, category = [], age = [], status } = req.body;
     let filters = {};
     if (seller) {
       filters.seller = seller;
     }
     if (status) {
       filters.status = status;
+    }
+
+    //filter by category
+    if (category.length > 0) {
+      filters.category = { $in: category };
+    }
+
+    //filter by age
+    if (age.length > 0) {
+      age.forEach((item) => {
+        const fromAge = item.split("-")[0];
+        const toAge = item.split("-")[1];
+        filters.age = { $gte: fromAge, $lte: toAge };
+      });
     }
     const products = await Product.find(filters)
       .populate("seller")
